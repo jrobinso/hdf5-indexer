4c91ac939824a27ed7ed2cba3fed00cecd330228
jrobinso <933148+jrobinso@users.noreply.github.com>
2023-01-17 21:54:29 -0800

    add setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='igv-reports',
-                 packages=['igv_reports'],
-                 version='1.6.1',
-                 description='Creates self-contained html pages for visual variant review with IGV (igv.js).',
+                 packages=['h5_indexer'],
+                 version='0.1.0',
+                 description='Creates an index of container -> file offset of HDF5 files.',
                  long_description=long_description,
                  long_description_content_type="text/markdown",
                  license='MIT',
@@ -14,21 +14,16 @@ setuptools.setup(name='igv-reports',
                  url='https://github.com/igvteam/igv-reports',
                  keywords=['igv', 'bioinformatics', 'genomics', 'visualization', 'variant' ],
                  classifiers=[
-                     'Programming Language :: Python :: 3',
-                     'Development Status :: 4 - Beta ',
-                     'Intended Audience :: Science/Research',
-                     'Intended Audience :: Developers',
                      'License :: OSI Approved :: MIT License',
-                     'Topic :: Scientific/Engineering :: Bio-Informatics '
+                     'Topic :: Scientific/Engineering :: Bio-Informatics :: HDF5'
                  ],
                  install_requires=[
-                     'pysam', 'intervalTree', 'requests'
+                     'pyfive==0.3.0', 'certifi'
                  ],
                  entry_points={
                      'console_scripts': [
-                         'create_report=igv_reports.report:main',
-                         'create_datauri=igv_reports.datauri:main'
+                         'h5index=h5_indexer.indexer:main',
                      ],
                  },
-                 package_data={'igv_reports': ['templates/*']},
+                 package_data={'h5_indexers': ['templates/*']},
                  )
