176493452646c194ba6d49a11f5a58145e1e990c
jrobinso <933148+jrobinso@users.noreply.github.com>
2024-02-21 12:54:52 -0800

    ..

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(name='hdf5-indexer',
                      'Topic :: Scientific/Engineering ::  HDF5'
                  ],
                  install_requires=[
-                    'pyfive', 'h5py', 'numpy'
+                     'pyfive @ git+https://github.com/jjhelmus/pyfive.git', 'h5py', 'numpy'
                  ],
                  dependency_links=["git+https://github.com/jjhelmus/pyfive.git@master#egg=pyfive"],
                  entry_points={
